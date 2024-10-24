2a11a43f448789c88478aefaefe22ff8f17b7416
Alan Eduardo Ramirez Salazar <chusterrs87@gmail.com>
2024-10-23 21:06:33 -0600

    Api1

---
+++ src/app/tem/addmessage/addmessage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addmessage',
+  standalone: true,
+  imports: [],
+  templateUrl: './addmessage.component.html',
+  styles: ``
+})
+export class AddmessageComponent {
+
+}
